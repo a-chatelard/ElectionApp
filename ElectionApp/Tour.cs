143c8212267fe84b4c9c92ea1757ceeae241612e
MarionPlayout <marion.playout@gmail.com>
2022-02-08 10:27:15 +0100

    Add PO decisions for part 2

--- ElectionApp/Tour.cs
+++ ElectionApp/Tour.cs
@@ -32,6 +32,13 @@ namespace ElectionApp
             return (float)voteCandidat / totalVote * 100;
         } 
 
+        /*
+         *Cas de l'égalité entre le 2eme et le 3eme candidat au premier tour : le tour est annulé.  
+         */
+
+        /*
+         *Cas du vote blanc : le vote blanc est comptabilisé. Si vote blanc >50% au premier tour, il n'y a pas de vainqueur au scrutin. Une nouvelle élection doit avoir lieu 
+         */
         public List<Candidat> GetVainqueur()
         {
             var vainqueurs = new List<Candidat>();
